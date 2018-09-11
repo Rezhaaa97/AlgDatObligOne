36bce1e3add76fa548e158256334bbacdbe83f38
Rejwan Hamarash Raouf <rejwanraouf@Rejwans-MacBook-Pro.local>
2018-09-11 10:53:15 +0200

    Initial commit

---
+++ src/Oblig1.java
@@ -0,0 +1,24 @@
+import java.util.NoSuchElementException;
+
+public class Oblig1 {
+
+    public static int maks(int [] a){
+        if (a.length == 0){
+            throw new NoSuchElementException("tabellen er tom");
+        }
+        int maks = a[0];
+        for (int i = 1; i <a.length ; i++) {
+            if(a[i]> maks){
+                maks = a[i];
+
+            }
+
+
+        }
+        return  maks;
+    }
+
+    public static int ombyttinger(int []a ){
+        return 1;
+    }
+}
